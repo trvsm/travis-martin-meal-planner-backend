d60333ccb87e42d1b15b9db402d7bf86d10fe97f
travis <trvsmartin@gmail.com>
2023-01-16 16:34:34 -0800

    add yarn for Render deploy
    <body>
    
    Issue #

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 const express = require("express");
 const fs = require("node:fs");
 const axios = require("axios");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 
-const recipeRouter = require('./routes/recipes.js');
+const recipeRouter = require("./routes/recipes.js");
 
 const arraySeparator = ",";
 // function to add bits to array to separate objects while writing
@@ -39,7 +39,7 @@ const buildJSON = (targetURL) => {
     });
     // syntaxInsertions(",");
   }
-//   syntaxInsertions("]");
+  //   syntaxInsertions("]");
 };
 
 /* ****NOTE: uncomment line below and run to make api call for more recipes**** */
@@ -47,7 +47,7 @@ const buildJSON = (targetURL) => {
 
 app.use(cors());
 
-app.use('/', recipeRouter)
+app.use("/", recipeRouter);
 
 const PORT = process.env.PORT || 1024;
 app.listen(PORT, () => {
